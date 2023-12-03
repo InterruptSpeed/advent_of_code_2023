56c4c3b4945e82bd907eb3098ad3fea1838994a1
InterruptSpeed <steven@docherty.ca>
2023-12-03 12:48:37 -0500

    finished day2/part2

--- day02/part2/main.cpp
+++ day02/part2/main.cpp
@@ -1,8 +1,8 @@
-// adventofcode.com/2023/day/2
+// adventofcode.com/2023/day/2#part2
 //$clang++ main.cpp --std=c++17 -o main
 //$./main
 
-// output should be 2810 for input.txt
+// output should be 69110 for input.txt
 
 #include <iostream>
 #include <fstream>
@@ -51,55 +51,70 @@ vector<string> extractTokens(const string str, const char delim = ':')
     return tokens;
 }
 
-struct Cubes {
-    size_t m_r;
-    size_t m_g;
-    size_t m_b;
+class Cubes {
+    private:
+        size_t m_r;
+        size_t m_g;
+        size_t m_b;
     
-    void update(const string colour, const size_t value) {
-        if (colour == "red") {
-            if (value > m_r)
-                m_r = value;
-        } else if (colour == "green") {
-            if (value > m_g)
-                m_g = value;
-        } else if (colour == "blue") {
-            if (value > m_b)
-                m_b = value;
-        } else {
-            cout << colour.length() << endl;
-            cout << colour << endl;
-            throw domain_error("invalid colour");
+    public:
+        Cubes(size_t r, size_t g, size_t b) : m_r{r}, m_g{g}, m_b{b} {}
+
+        void update(const string colour, const size_t value) {
+            if (colour == "red") {
+                if (value > m_r)
+                    m_r = value;
+            } else if (colour == "green") {
+                if (value > m_g)
+                    m_g = value;
+            } else if (colour == "blue") {
+                if (value > m_b)
+                    m_b = value;
+            } else {
+                cout << colour.length() << endl;
+                cout << colour << endl;
+                throw domain_error("invalid colour");
+            }
         }
-    }
 
-    friend ostream& operator<<(ostream& os, const Cubes& cubes) {
-        os << cubes.m_r << " red, " << cubes.m_g << " green, and " << cubes.m_b << " blue cubes";
-        return os;
-    }
+        size_t getR() { return m_r; }
+        size_t getG() { return m_g; }
+        size_t getB() { return m_b; }
 
-    friend bool operator<=(const Cubes& lhs, const Cubes& rhs)
-    {
-        return (lhs.m_r <= rhs.m_r) && (lhs.m_g <= rhs.m_g) && (lhs.m_b <= rhs.m_b);
-    }
+        size_t getPower() {
+            return m_r * m_g * m_b;
+        }
 
-    friend bool operator==(const Cubes& lhs, const Cubes& rhs)
-    {
-        return (lhs.m_r == rhs.m_r) && (lhs.m_g == rhs.m_g) && (lhs.m_b == rhs.m_b);
-    }
+        friend ostream& operator<<(ostream& os, const Cubes& cubes) {
+            os << cubes.m_r << " red, " << cubes.m_g << " green, and " << cubes.m_b << " blue cubes for a total power of " << endl;
+            return os;
+        }
+
+        friend bool operator<=(const Cubes& lhs, const Cubes& rhs)
+        {
+            return (lhs.m_r <= rhs.m_r) && (lhs.m_g <= rhs.m_g) && (lhs.m_b <= rhs.m_b);
+        }
+
+        friend bool operator==(const Cubes& lhs, const Cubes& rhs)
+        {
+            return (lhs.m_r == rhs.m_r) && (lhs.m_g == rhs.m_g) && (lhs.m_b == rhs.m_b);
+        }
 };
 
 class Bag {
     public:
-        Bag(size_t game_id, size_t num_reveals, Cubes cubes) : m_game_id(game_id), m_num_reveals(num_reveals), m_cubes_revealed(cubes){}
+        Bag(size_t game_id, size_t num_reveals, Cubes cubes_revealed) : m_game_id(game_id), m_num_reveals(num_reveals), m_cubes_revealed(cubes_revealed){}
         static Bag create(const string line);
         friend ostream& operator<<(ostream& os, const Bag& bag);
-        friend bool operator<=(const Bag& lhs, const Bag& rhs) { return lhs.m_cubes_revealed <= rhs.m_cubes_revealed; }
-        friend bool operator==(const Bag& lhs, const Bag& rhs) { return lhs.m_cubes_revealed == rhs.m_cubes_revealed; }
 
         auto getGameID() {
             return m_game_id;
         }
+
+        auto getCubesRevealed() {
+            return m_cubes_revealed;
+        }
+
     private:
         size_t m_game_id;
         size_t m_num_reveals;
@@ -108,7 +123,7 @@ class Bag {
 
 Bag Bag::create(const string line) {
     size_t id = 0;
-    Cubes cubes{0, 0, 0};
+    Cubes cubes_revealed{0, 0, 0};
 
     vector<string> tokens = extractTokens(line);
     if (tokens.size() != 2)
@@ -138,14 +153,14 @@ Bag Bag::create(const string line) {
         auto samples = extractTokens(reveal, ',');
         for (auto sample: samples) {
             auto qty_clr = extractTokens(sample, ' ');
-            cubes.update(qty_clr.back(), stoi(qty_clr.front()));
+            cubes_revealed.update(qty_clr.back(), stoi(qty_clr.front()));
         }
     }
-    return Bag{id, tokens.size(), cubes};
+    return Bag{id, tokens.size(), cubes_revealed};
 }
 
 ostream& operator<<(ostream& os, const Bag& bag) {
-    os << "\tAfter " << bag.m_num_reveals << " reveals, the bag in game " << bag.m_game_id << " contains " << bag.m_cubes_revealed << ".";
+    os << "\tAfter " << bag.m_num_reveals << " reveals, the bag in game " << bag.m_game_id << " contains minimum " << bag.m_cubes_revealed << ".";
     return os;
 }
 
@@ -176,17 +191,21 @@ int main()
     Bag elf_bag{0, 1, Cubes{12, 13, 14}};
 
     size_t sum_of_ids = 0;
+    size_t sum_of_power = 0;
     while (getline(myfile, line))
     {
         cout << line << endl;
         auto bag = Bag::create(line);
         cout << bag << endl;
-        if (bag <= elf_bag)
+        cout << bag.getCubesRevealed().getPower() << endl;
+        if (bag.getCubesRevealed() <= elf_bag.getCubesRevealed())
             sum_of_ids += bag.getGameID();
+            sum_of_power += bag.getCubesRevealed().getPower();
     }
     myfile.close();
     
     cout << "sum of possible ids: " << sum_of_ids << endl;
+    cout << "sum of power: " << sum_of_power << endl;
 
     return 0;
 }
